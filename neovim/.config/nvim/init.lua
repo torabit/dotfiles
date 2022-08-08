ad48189f276ce40875430f9389dcf71a5f83b06e
torabit <pujigami@gmail.com>
2022-08-09 00:04:41 +0900

    ADD init neovim

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,129 @@
+require 'impatient'
+local g = vim.g
+local cmd = vim.cmd
+local o, wo, bo = vim.o, vim.wo, vim.bo
+local utils = require 'config.utils'
+local opt = utils.opt
+local autocmd = utils.autocmd
+local map = utils.map
+
+-- Leader/local leader
+g.mapleader = [[ ]]
+g.maplocalleader = [[,]]
+
+-- Disable some built-in plugins we don't want
+local disabled_built_ins = {
+  'gzip',
+  'man',
+  'matchit',
+  'matchparen',
+  'shada_plugin',
+  'tarPlugin',
+  'tar',
+  'zipPlugin',
+  'zip',
+  'netrwPlugin',
+}
+
+for i = 1, 10 do
+  g['loaded_' .. disabled_built_ins[i]] = 1
+end
+
+-- Settings
+local buffer = { o, bo }
+local window = { o, wo }
+opt('textwidth', 100, buffer)
+opt('scrolloff', 7)
+opt('wildignore', '*.o,*~,*.pyc')
+opt('wildmode', 'longest,full')
+opt('whichwrap', vim.o.whichwrap .. '<,>,h,l')
+opt('inccommand', 'nosplit')
+opt('lazyredraw', true)
+opt('showmatch', true)
+opt('ignorecase', true)
+opt('smartcase', true)
+opt('tabstop', 2, buffer)
+opt('softtabstop', 0, buffer)
+opt('expandtab', true, buffer)
+opt('shiftwidth', 2, buffer)
+opt('number', true, window)
+opt('relativenumber', true, window)
+opt('smartindent', true, buffer)
+opt('laststatus', 3)
+opt('showmode', false)
+opt('shada', [['20,<50,s10,h,/100]])
+opt('hidden', true)
+opt('shortmess', o.shortmess .. 'c')
+opt('joinspaces', false)
+opt('guicursor', [[n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50]])
+opt('updatetime', 500)
+opt('conceallevel', 2, window)
+opt('concealcursor', 'nc', window)
+opt('previewheight', 5)
+opt('undofile', true, buffer)
+opt('synmaxcol', 500, buffer)
+opt('display', 'msgsep')
+opt('cursorline', true, window)
+opt('modeline', false, buffer)
+opt('mouse', 'nivh')
+opt('signcolumn', 'yes:1', window)
+
+-- Autocommands
+autocmd('start_screen', [[VimEnter * ++once lua require('start').start()]], true)
+autocmd(
+  'syntax_aucmds',
+  [[Syntax * syn match extTodo "\<\(NOTE\|HACK\|BAD\|TODO\):\?" containedin=.*Comment.* | hi! link extTodo Todo]],
+  true
+)
+autocmd('misc_aucmds', {
+  [[BufWinEnter * checktime]],
+  [[TextYankPost * silent! lua vim.highlight.on_yank()]],
+  [[FileType qf set nobuflisted ]],
+}, true)
+
+-- Commands
+cmd [[command! WhatHighlight :call util#syntax_stack()]]
+cmd [[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]]
+cmd [[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]]
+cmd [[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]]
+cmd [[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]]
+cmd [[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]]
+
+-- Keybindings
+local silent = { silent = true }
+-- Disable annoying F1 binding
+map('', '<f1>', '<cmd>FloatermToggle<cr>')
+
+-- Run a build
+map('n', '<localleader><localleader>', '<cmd>Make<cr>', silent)
+
+-- Quit, close buffers, etc.
+map('n', '<leader>q', '<cmd>qa<cr>', silent)
+map('n', '<leader>x', '<cmd>x!<cr>', silent)
+map('n', '<leader>d', '<cmd>Sayonara<cr>', { silent = true, nowait = true })
+
+-- A little Emacs in my Neovim
+map('i', '<c-s>', '<esc><cmd>w<cr>a', silent)
+
+-- Save buffer
+map('n', '<leader>w', '<cmd>w<cr>', { silent = true })
+
+-- Version control
+map('n', 'gs', '<cmd>Neogit<cr>', silent)
+
+-- Esc in the terminal
+map('t', 'jj', [[<C-\><C-n>]])
+map('i', 'jj', '<esc>', silent)
+
+-- Yank to clipboard
+map({ 'n', 'v' }, 'y+', '<cmd>set opfunc=util#clipboard_yank<cr>g@', silent)
+
+-- Window movement
+map('n', '<c-h>', '<c-w>h')
+map('n', '<c-j>', '<c-w>j')
+map('n', '<c-k>', '<c-w>k')
+map('n', '<c-l>', '<c-w>l')
+
+-- Tab movement
+map('n', '<c-Left>', '<cmd>tabpre<cr>')
+map('n', '<c-Right>', '<cmd>tabnext<cr>')
---
+++ neovim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,272 @@
+local packer = nil
+local function init()
+  if packer == nil then
+    packer = require 'packer'
+    packer.init { disable_commands = true }
+  end
+
+  local use = packer.use
+  packer.reset()
+
+  -- Packer
+  use 'wbthomason/packer.nvim'
+
+  use 'lewis6991/impatient.nvim'
+
+  -- Async building & commands
+  use { 'tpope/vim-dispatch', cmd = { 'Dispatch', 'Make', 'Focus', 'Start' } }
+
+  -- Registers
+    use 'mhinz/vim-sayonara'
+
+  -- Marks
+  use { 'kshenoy/vim-signature', config = [[require('config.signature')]], disable = true }
+
+  use { 'tversteeg/registers.nvim', keys = { { 'n', '"' }, { 'i', '<c-r>' } } }
+
+  -- Movement
+  use { 'chaoren/vim-wordmotion', 'justinmk/vim-sneak' }
+
+  -- Quickfix
+  use { 'Olical/vim-enmasse', cmd = 'EnMasse' }
+  use 'kevinhwang91/nvim-bqf'
+
+  -- Indentation tracking
+  use 'lukas-reineke/indent-blankline.nvim'
+
+  -- Commenting
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end,
+  }
+
+  -- Wrapping/delimiters
+  use {
+    'machakann/vim-sandwich',
+    { 'andymass/vim-matchup', setup = [[require('config.matchup')]], event = 'User ActuallyEditing' },
+  }
+
+  -- Search
+  use 'romainl/vim-cool'
+
+  -- Prettification
+  use 'junegunn/vim-easy-align'
+
+  -- Text objects
+  use 'wellle/targets.vim'
+
+  -- Search
+  use {
+    {
+      'nvim-telescope/telescope.nvim',
+      requires = {
+        'nvim-lua/popup.nvim',
+        'nvim-lua/plenary.nvim',
+        'telescope-frecency.nvim',
+        'telescope-fzf-native.nvim',
+        'nvim-telescope/telescope-ui-select.nvim',
+      },
+      wants = {
+        'popup.nvim',
+        'plenary.nvim',
+        'telescope-frecency.nvim',
+        'telescope-fzf-native.nvim',
+      },
+      setup = [[require('config.telescope_setup')]],
+      config = [[require('config.telescope')]],
+      cmd = 'Telescope',
+      module = 'telescope',
+    },
+    {
+      'nvim-telescope/telescope-frecency.nvim',
+      after = 'telescope.nvim',
+      requires = 'tami5/sqlite.lua',
+    },
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      run = 'make',
+    },
+  }
+
+  -- Project Management/Sessions
+  use {
+    'dhruvasagar/vim-prosession',
+    after = 'vim-obsession',
+    requires = { { 'tpope/vim-obsession', cmd = 'Prosession' } },
+    config = [[require('config.prosession')]],
+  }
+
+  -- Undo tree
+  use {
+    'mbbill/undotree',
+    cmd = 'UndotreeToggle',
+    config = [[vim.g.undotree_SetFocusWhenToggle = 1]],
+  }
+
+  -- Git
+  use {
+    { 'tpope/vim-fugitive', cmd = { 'Git', 'Gstatus', 'Gblame', 'Gpush', 'Gpull' }, disable = true },
+    {
+      'lewis6991/gitsigns.nvim',
+      requires = { 'nvim-lua/plenary.nvim' },
+      config = [[require('config.gitsigns')]],
+    },
+    { 'TimUntersberger/neogit', cmd = 'Neogit', config = [[require('config.neogit')]] },
+  }
+
+  -- Pretty symbols
+  use 'kyazdani42/nvim-web-devicons'
+
+  -- REPLs
+  use {
+    'hkupty/iron.nvim',
+    setup = [[vim.g.iron_map_defaults = 0]],
+    config = [[require('config.iron')]],
+    cmd = { 'IronRepl', 'IronSend', 'IronReplHere' },
+  }
+
+  -- Completion and linting
+  use {
+    'neovim/nvim-lspconfig',
+    'nvim-lua/lsp-status.nvim',
+    'folke/trouble.nvim',
+    'ray-x/lsp_signature.nvim',
+    'kosayoda/nvim-lightbulb',
+  }
+
+  -- C++
+  use 'p00f/clangd_extensions.nvim'
+
+  -- Highlights
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    requires = {
+      'nvim-treesitter/nvim-treesitter-refactor',
+      'RRethy/nvim-treesitter-textsubjects',
+    },
+    run = ':TSUpdate',
+  }
+
+  -- Documentation
+  use {
+    'danymat/neogen',
+    requires = 'nvim-treesitter',
+    config = [[require('config.neogen')]],
+    keys = { '<localleader>d', '<localleader>df', '<localleader>dc' },
+  }
+
+  -- Lisps
+  use 'gpanders/nvim-parinfer'
+
+  -- Completion
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'L3MON4D3/LuaSnip',
+      { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lua', after = 'nvim-cmp' },
+      { 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp' },
+      'lukas-reineke/cmp-under-comparator',
+      { 'hrsh7th/cmp-nvim-lsp-document-symbol', after = 'nvim-cmp' },
+    },
+    config = [[require('config.cmp')]],
+    event = 'InsertEnter *',
+  }
+
+  -- Endwise
+  use 'RRethy/nvim-treesitter-endwise'
+
+  -- Debugger
+  use {
+    {
+      'mfussenegger/nvim-dap',
+      setup = [[require('config.dap_setup')]],
+      config = [[require('config.dap')]],
+      requires = 'jbyuki/one-small-step-for-vimkind',
+      wants = 'one-small-step-for-vimkind',
+      module = 'dap',
+    },
+    {
+      'rcarriga/nvim-dap-ui',
+      requires = 'nvim-dap',
+      after = 'nvim-dap',
+      config = function()
+        require('dapui').setup()
+      end,
+    },
+  }
+
+  use {
+    'puremourning/vimspector',
+    setup = [[vim.g.vimspector_enable_mappings = 'HUMAN']],
+    disable = true,
+  }
+
+  -- Path navigation
+  use 'justinmk/vim-dirvish'
+
+  -- LaTeX
+  use 'lervag/vimtex'
+
+  -- Meson
+  use 'igankevich/mesonic'
+
+  -- PDDL
+  use 'PontusPersson/pddl.vim'
+
+  -- Zig
+  use 'ziglang/zig.vim'
+
+  -- Julia
+  use { 'JuliaEditorSupport/julia-vim', setup = [[vim.g.latex_to_unicode_tab = 'off']], opt = true }
+
+  -- Profiling
+  use { 'dstein64/vim-startuptime', cmd = 'StartupTime', config = [[vim.g.startuptime_tries = 10]] }
+
+  -- Refactoring
+  use 'ThePrimeagen/refactoring.nvim'
+  -- Plugin development
+  use 'folke/lua-dev.nvim'
+
+  -- Highlight colors
+  use {
+    'norcalli/nvim-colorizer.lua',
+    ft = { 'css', 'javascript', 'vim', 'html' },
+    config = [[require('colorizer').setup {'css', 'javascript', 'vim', 'html'}]],
+  }
+
+  -- Notes
+  use {
+    '~/projects/personal/pdf-scribe.nvim',
+    config = [[require('config.pdf_scribe')]],
+    disable = true,
+  }
+
+  use { { 'nvim-orgmode/orgmode.nvim', config = [[require('config.orgmode')]] }, 'akinsho/org-bullets.nvim' }
+
+  -- Buffer management
+  use {
+    'akinsho/bufferline.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = [[require('config.bufferline')]],
+    event = 'User ActuallyEditing',
+  }
+
+  use 'b0o/incline.nvim'
+  use 'teal-language/vim-teal'
+  use { 'jose-elias-alvarez/null-ls.nvim', requires = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' } }
+end
+
+local plugins = setmetatable({}, {
+  __index = function(_, key)
+    init()
+    return packer[key]
+  end,
+})
+
+return plugins
