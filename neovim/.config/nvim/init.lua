6fa6c4b0d87bef3d6a84d1de142e9e377c7a845c
torabit <pujigami@gmail.com>
2023-03-28 20:28:37 +0900

    Update notify settings

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -4,6 +4,8 @@ require 'maps'
 require 'plugins'
 vim.notify = require 'notify'
 
+require('notify').setup { background_colour = '#000000' }
+
 local has = vim.fn.has
 local is_mac = has "macunix"
 local is_wsl = has "wsl"
