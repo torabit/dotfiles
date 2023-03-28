215726a84aa7544d608897b711cb2479d8a1afa5
torabit <pujigami@gmail.com>
2023-03-28 20:31:19 +0900

    Update merge null-ls conf into lspconf

--- neovim/.config/nvim/plugin/lspconfig.rc.lua
+++ neovim/.config/nvim/plugin/lspconfig.rc.lua
@@ -1,11 +1,69 @@
-local status, lspconfig = pcall(require, 'lspconfig')
-if (not status) then return end
+---@diagnostic disable: unused-local
+local lspconfig = require 'lspconfig'
+local trouble = require 'trouble'
+local lsp_signature = require 'lsp_signature'
+local null_ls = require 'null-ls'
 
-local protocol = require('vim.lsp.protocol')
+local lsp = vim.lsp
+local buf_keymap = vim.api.nvim_buf_set_keymap
+local cmd = vim.cmd
 
-local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+local sign_define = vim.fn.sign_define
+sign_define('DiagnosticSignError', { text = '', numhl = 'RedSign' })
+sign_define('DiagnosticSignWarn', { text = '', numhl = 'YellowSign' })
+sign_define('DiagnosticSignInfo', { text = '', numhl = 'WhiteSign' })
+sign_define('DiagnosticSignHint', { text = '', numhl = 'BlueSign' })
+trouble.setup()
 
-local on_attach = function(client, bufnr)
+vim.diagnostic.config { virtual_lines = { only_current_line = true }, virtual_text = false }
+lsp.handlers['textDocument/publishDiagnostics'] = lsp.with(lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = false,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+})
+
+lsp_signature.setup { bind = true, handler_opts = { border = 'single' } }
+local keymap_opts = { noremap = true, silent = true }
+local function on_attach(client)
+  require('lsp_signature').on_attach { bind = true, handler_opts = { border = 'single' } }
+  buf_keymap(0, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', keymap_opts)
+  buf_keymap(0, 'n', 'gd', '<cmd>lua require"telescope.builtin".lsp_definitions()<CR>', keymap_opts)
+  buf_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', keymap_opts)
+  buf_keymap(0, 'n', 'gi', '<cmd>lua require"telescope.builtin".lsp_implementations()<CR>', keymap_opts)
+  buf_keymap(0, 'n', 'gS', '<cmd>lua vim.lsp.buf.signature_help()<CR>', keymap_opts)
+  buf_keymap(0, 'n', 'gTD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', keymap_opts)
+  buf_keymap(0, 'n', '<leader>rn', '<cmd>lua require"renamer".rename()<CR>', keymap_opts)
+  buf_keymap(0, 'v', '<leader>rn', '<cmd>lua require"renamer".rename()<CR>', keymap_opts)
+  buf_keymap(0, 'n', 'gr', '<cmd>lua require"telescope.builtin".lsp_references()<CR>', keymap_opts)
+  buf_keymap(0, 'n', 'gA', '<cmd>lua vim.lsp.buf.code_action()<CR>', keymap_opts)
+  buf_keymap(0, 'v', 'gA', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', keymap_opts)
+  buf_keymap(0, 'n', ']e', '<cmd>lua vim.diagnostic.goto_next { float = {scope = "line"} }<cr>', keymap_opts)
+  buf_keymap(0, 'n', '[e', '<cmd>lua vim.diagnostic.goto_prev { float = {scope = "line"} }<cr>', keymap_opts)
+
+  if client.supports_method('textDocument/formatting') then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end
+    })
+  end
+
+  if client.server_capabilities.documentFormattingProvider then
+    buf_keymap(0, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.format { async = true }<cr>', keymap_opts)
+  end
+
+  cmd 'augroup lsp_aucmds'
+  if client.server_capabilities.documentHighlightProvider then
+    cmd 'au CursorHold <buffer> lua vim.lsp.buf.document_highlight()'
+    cmd 'au CursorMoved <buffer> lua vim.lsp.buf.clear_references()'
+  end
+
+  cmd 'augroup END'
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
@@ -17,9 +75,7 @@ lspconfig.gopls.setup {
 
 lspconfig.lua_ls.setup {
   capabilities = capabilities,
-  on_attach = function(client, bufnr)
-    on_attach(client, bufnr)
-  end,
+  on_attach = on_attach,
   settings = {
     Lua = {
       diagnostics = {
@@ -35,19 +91,16 @@ lspconfig.lua_ls.setup {
   },
 }
 
--- Diagnostic symbols in the sign column (gutter)
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
+-- null-ls setup
+local null_fmt = null_ls.builtins.formatting
+local null_diag = null_ls.builtins.diagnostics
+local null_act = null_ls.builtins.code_actions
 
-vim.diagnostic.config({
-  virtual_text = {
-    prefix = '●'
+null_ls.setup {
+  on_attach =  on_attach,
+  sources = {
+    null_diag.shellcheck,
+    null_fmt.gofmt,
+    null_act.gitsigns,
   },
-  update_in_insert = true,
-  float = {
-    source = "always", -- Or "if_many"
-  },
-})
+}
