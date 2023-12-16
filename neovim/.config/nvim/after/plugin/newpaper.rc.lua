0a188d0974fc1ec2ef1fd88f71b7ed43cb7c5a43
torabit <pujigami@gmail.com>
2023-12-16 21:29:30 +0900

    feat(neovim): use lazy git

--- neovim/.config/nvim/after/plugin/newpaper.rc.lua
+++
@@ -1,9 +0,0 @@
-local status, newpaper = pcall(require, 'newpaper')
-if (not status) then return end
-
-newpaper.setup {
-  style = 'light',
-  disable_background = true,
-  italic_strings = false,
-  italic_comments = false,
-}
--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,15 +1,2 @@
-require 'base'
-require 'highlights'
-require 'maps'
-require 'plugins'
-
-local has = vim.fn.has
-local is_mac = has "macunix"
-local is_wsl = has "wsl"
-
-if is_mac then
-  require 'macos'
-end
-if is_wsl then
-  require 'wsl'
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
