f27a24003c87d53d12aa218fba34e4162eb2d8ec
torabit <pujigami@gmail.com>
2023-12-18 01:00:20 +0900

    feat(nvim): render dashboard status for lualine :sparkles:

---
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = function(_, opts)
+    table.insert(opts.sections.lualine_x, {
+      function()
+        return require("torabit.dashboard").status()
+      end,
+    })
+  end,
+}
